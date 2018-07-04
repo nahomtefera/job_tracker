efbf28221f9795cc2c58da364b84d6bfa53d757e
nahom <nahumtefera@gmail.com>
2018-07-03 18:32:59 -0700

    created Job component

---
+++ src/components/job/job.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './job.css';
+
+class Job extends Component {
+
+    render(){
+        return(
+            <div className="job-container">
+                
+            </div>
+        )
+    }
+}
+
+export default Job;
