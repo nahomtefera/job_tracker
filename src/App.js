6898bb77b468582fbae1a81e49ba170711496bc1
nahom <nahumtefera@gmail.com>
2018-07-04 14:33:13 -0700

    stoped using Job Component, changed state job property names, created handleChange method

--- src/App.js
+++ src/App.js
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
-import Job from './components/job/job';
+// import Job from './components/job/job';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      /*
+        id
+        title, company, location, date
+        contact: name, email, phone
+        phone_interview: date, time, follow_up, thanks
+        skype_interview: date, time, follow_up, thanks
+        onsite_interview: date, time, follow_up, thanks
+        offer
+        */ 
+      jobs: [
+        {
+          id: 1,
+          title: "",
+          company: "",
+          location: "",
+          date: "",
+          contact_name: "",
+          contact_emal: "",
+          contact_phone: "",
+          phone_interview_date: "",
+          phone_interview_time: "",
+          phone_interview_follow: false,
+          phone_interview_thanks: false,
+          skype_interview_date: "",
+          skype_interview_time: "",
+          skype_interview_follow: false,
+          skype_interview_thanks: false,
+          onsite_interview_date: "",
+          onsite_interview_time: "",
+          onsite_interview_follow: false,
+          onsite_interview_thanks: false,
+          offer: ""
+        },
+      ],
+      num_of_jobs: 1
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(el) {
+    let val = el.target.value;
+    let el_class = el.target.className;
+    let parent_id = el.target.parentNode.id;
+    let prevState = this.state.jobs;
+    
+    for(let i=0; i<prevState.length; i++){
+      if(Number(parent_id) === Number(prevState[i].id)){
+        prevState[i][el_class] = val
+      }
+    }
+
+    this.setState({
+      jobs: prevState
+    })
+  } 
+
   render() {
     return (
       <div className="App">
-        <Job />
+        {this.state.jobs.map((job)=>{
+          return (
+            <div id={job.id} key={"job-" + job.id}>
+                <input className="title" placeholder="Job Title" value={job.title} onChange={this.handleChange} /> <br/>
+                <input className="company" placeholder="Company" value={job.company} onChange={this.handleChange} /> <br/>
+                <input className="location" placeholder="Location" value={job.location} onChange={this.handleChange} /> <br/>
+                <input className="date" placeholder="Date" value={job.date} onChange={this.handleChange} /> <br/>
+
+            </div>
+          )
+        })}
       </div>
     );
   }
--- src/components/job/job.jsx
+++ src/components/job/job.jsx
@@ -3,10 +3,22 @@ import './job.css';
 
 class Job extends Component {
 
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            job: this.props.job
+        }
+    }
+
     render(){
         return(
             <div className="job-container">
-                
+                {this.state.job.title} <br/>
+                {this.state.job.company} <br/>
+                {this.state.job.location} <br/>
+                {this.state.job.date} <br/>
+
             </div>
         )
     }
